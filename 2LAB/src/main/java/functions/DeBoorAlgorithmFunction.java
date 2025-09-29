a49ef193c4bf4565984f81b727fe504747b80ae4
line4kk's book <silandr2016@gmail.com>
2025-09-29 16:14:42 +0400

    Теперь алгоритм Де-Бура работает правильно. Добавлены тесты

--- 2LAB/src/main/java/functions/DeBoorAlgorithmFunction.java
+++ 2LAB/src/main/java/functions/DeBoorAlgorithmFunction.java
@@ -6,26 +6,39 @@ public class DeBoorAlgorithmFunction implements MathFunction {
     private final int degree;  // Степень (p)
 
     public DeBoorAlgorithmFunction(double[][] controlPoints, double[] knots, int degree) {
+        // ДОЛЖНО ВЫПОЛНЯТЬСЯ knots.length = controlPoints.length + degree + 1.
+        // ИНАЧЕ АЛГОРИТМ НЕ РАБОТАЕТ
         this.controlPoints = controlPoints;
         this.knots = knots;
         this.degree = degree;
     }
 
     private int findSegment(double x) {  // Находим номер отрезка k, такой, что x принад. [t[k]; t[k+1])
-        /*if (x == knots[knots.length - 1])
-            return knots.length - 2;*/
-        int k = 0;
+        int k = -1;
+        int n = controlPoints.length - 1;
 
-        while ((k < knots.length - 1) && !(x >= knots[k] && x < knots[k + 1]) && !(knots[k] != knots[k+1]))
-            k++;
+        for (int i = degree; i <= n; i++) {
+            if (x >= knots[i] && x < knots[i + 1]) {
+                k = i;
+                break;
+            }
+        }
+        if (k == -1)
+            k = n;
 
         return k;
+
     }
 
-    private double alpha(double x, int k, int j) {
-        return (x - knots[j + k - degree]) / (knots[j + 1 + k - degree] - knots[j + k - degree]);
+    private double alpha(double x, int k, int j, int r) {
+        double denom = knots[j + 1 + k - r] - knots[j + k - degree];
+        if (denom == 0.0) {
+            return 0.0; // по определению
+        }
+        return (x - knots[j + k - degree]) / denom;
     }
 
+    @Override
     public double apply(double x) {
         double[] resultPoints = new double[controlPoints.length];
         int k = findSegment(x);
@@ -34,7 +47,7 @@ public class DeBoorAlgorithmFunction implements MathFunction {
 
         for (int r = 1; r <= degree; r++) {
             for (int j = degree; j >= r; j--) {
-                resultPoints[j] = (1 - alpha(x, k, j)) * resultPoints[j - 1] + alpha(x, k, j) * resultPoints[j];
+                resultPoints[j] = (1 - alpha(x, k, j, r)) * resultPoints[j - 1] + alpha(x, k, j, r) * resultPoints[j];
             }
         }
         return resultPoints[degree];
