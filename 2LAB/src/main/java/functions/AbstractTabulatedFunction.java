97ab3eb43469c522d081cc1c852bcfe1082a82e1
Данила Рудяга <rudyaga2005@gmail.com>
2025-09-29 19:17:55 +0400

    Реализована функция AbstractTabulatedFunction

---
+++ 2LAB/src/main/java/functions/AbstractTabulatedFunction.java
@@ -0,0 +1,51 @@
+package functions;
+
+// Абстрактный класс для табличных функций, реализующий общую логику интерполяции и экстраполяции
+public abstract  class AbstractTabulatedFunction implements TabulatedFunction{
+
+    protected int count; // Приватное поле count
+
+    protected abstract int floorIndexOfX(double x); // поиск интеравала x
+    protected abstract double extrapolateLeft(double x); // экстраполяция слева
+    protected abstract double extrapolateRight(double x); // экстраполяция справа
+    protected abstract double interpolate(double x, int floorIndex); // интерполяция с указанием индекса интервала
+
+    // Заищённый метод с реализацией интерполяции
+    protected double interpolate(double x, double leftX, double rightX,
+                                 double leftY, double rightY) {
+        return leftY + (rightY - leftY) * (x - leftX) / (rightX - leftX);
+    }
+
+    //Абстрактные методы из TabulatedFunction
+    public abstract int getCount();  // Метод получения количества табулированных значений
+    public abstract double getX(int index);  // Метод, получающий значение аргумента x по номеру индекса
+    public abstract double getY(int index);  // Метод, получающий значение y по номеру индекса:
+    public abstract void setY(int index, double value);  // Метод, задающий значение y по номеру индекса
+    public abstract int indexOfX(double x);  // Метод, возвращающий индекс аргумента x. Предполагается, что все x различны. Если такого x в таблице нет, то необходимо вернуть -1
+    public abstract int indexOfY(double y);  // Метод, возвращающий индекс первого вхождения значения y. Если такого y в таблице нет, то необходимо вернуть -1:
+    public abstract double leftBound();  // Метод, возвращающий самый левый x
+    public abstract double rightBound();  // Метод, возвращающий самый правый x
+
+    @Override
+    public double apply(double x) {
+        // x меньше левой границы
+        if (x < getX(0)) {
+            return extrapolateLeft(x);
+        }
+
+        // x больше правой границы
+        if (x > getX(count - 1)) {
+            return extrapolateRight(x);
+        }
+
+        // x внутри интервала
+        int exactIndex = indexOfX(x);
+        if (exactIndex != -1) {
+            return getY(exactIndex);
+        }
+
+        // Вызов метода интерполяции
+        int floorIndex = floorIndexOfX(x);
+        return interpolate(x, floorIndex);
+    }
+}
