7d5671bdac8b6bd80ff43f4c71049c23c8aa35ff
line4kk's book <silandr2016@gmail.com>
2025-10-13 22:21:53 +0400

    writeTabulatedFunction output в FunctionsIO

--- 2LAB/src/main/java/io/FunctionsIO.java
+++ 2LAB/src/main/java/io/FunctionsIO.java
@@ -1,7 +1,27 @@
 package io;
 
+import functions.Point;
+import functions.TabulatedFunction;
+import operations.TabulatedFunctionOperationService;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException("Нельзя создать экземпляр служебного класса");
+    }
+
+    public static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        try (DataOutputStream stream = new DataOutputStream(outputStream)) {
+            Point[] points = TabulatedFunctionOperationService.asPoints(function);
+            stream.writeInt(function.getCount());
+            for (Point point : points) {
+                stream.writeDouble(point.x);
+                stream.writeDouble(point.y);
+            }
+            stream.flush();
+        }
     }
 }
