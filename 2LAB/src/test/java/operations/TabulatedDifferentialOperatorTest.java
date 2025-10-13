c04973a312cb09d23cdc5a464282faded8b1b462
line4kk's book <silandr2016@gmail.com>
2025-10-13 18:36:23 +0400

    Оператор дифференцирования TabulatedDifferentialOperator. Тесты

---
+++ 2LAB/src/test/java/operations/TabulatedDifferentialOperatorTest.java
@@ -0,0 +1,51 @@
+package operations;
+
+import functions.ArrayTabulatedFunction;
+import functions.LinkedListTabulatedFunction;
+import functions.TabulatedFunction;
+import functions.factory.ArrayTabulatedFunctionFactory;
+import functions.factory.LinkedListTabulatedFunctionFactory;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TabulatedDifferentialOperatorTest {
+
+    @Test
+    void testDeriveArray() {
+        ArrayTabulatedFunctionFactory factory = new ArrayTabulatedFunctionFactory();
+        TabulatedDifferentialOperator operator = new TabulatedDifferentialOperator(factory);
+
+        double[] xValues = {0.0, 1.0, 2.0, 4.0};  // y = 3x + 1; y' = 3
+        double[] yValues = {1.0, 4.0, 7.0, 13.0};
+
+        ArrayTabulatedFunction fun = new ArrayTabulatedFunction(xValues, yValues);
+
+        TabulatedFunction derivativeFun = operator.derive(fun);
+
+        assertEquals(3.0, derivativeFun.getY(0));
+        assertEquals(3.0, derivativeFun.getY(1));
+        assertEquals(3.0, derivativeFun.getY(2));
+        assertEquals(3.0, derivativeFun.getY(3));
+
+    }
+
+    @Test
+    void testDeriveLinkedList() {
+        LinkedListTabulatedFunctionFactory factory = new LinkedListTabulatedFunctionFactory();
+        TabulatedDifferentialOperator operator = new TabulatedDifferentialOperator(factory);
+
+        double[] xValues = {0.0, 1.0, 2.0, 5.0};  // y = 77x; y' = 77
+        double[] yValues = {0.0, 77.0, 154.0, 385.0};
+
+        LinkedListTabulatedFunction fun = new LinkedListTabulatedFunction(xValues, yValues);
+
+        TabulatedFunction derivativeFun = operator.derive(fun);
+
+        assertEquals(77.0, derivativeFun.getY(0));
+        assertEquals(77.0, derivativeFun.getY(1));
+        assertEquals(77.0, derivativeFun.getY(2));
+        assertEquals(77.0, derivativeFun.getY(3));
+
+    }
+}
