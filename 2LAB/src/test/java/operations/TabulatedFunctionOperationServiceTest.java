983a093d1b223614bf994d82424c71c3830e247e
line4kk's book <silandr2016@gmail.com>
2025-10-13 20:18:15 +0400

    Добавлены тесты для умножения и деления функций

--- 2LAB/src/test/java/operations/TabulatedFunctionOperationServiceTest.java
+++ 2LAB/src/test/java/operations/TabulatedFunctionOperationServiceTest.java
@@ -161,4 +161,285 @@ class TabulatedFunctionOperationServiceTest {
 
 
     }
+
+    @Test
+    void testMultiplicationWithArrayTabulatedFunctions() {
+        double[] xValues1 = {1.0, 2.0, 3.0};
+        double[] yValues1 = {5.0, 10.0, 15.0};
+        double[] xValues2 = {1.0, 2.0, 3.0};
+        double[] yValues2 = {1.0, 2.0, 3.0};
+
+        ArrayTabulatedFunction func1 = new ArrayTabulatedFunction(xValues1, yValues1);
+        ArrayTabulatedFunction func2 = new ArrayTabulatedFunction(xValues2, yValues2);
+
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService();
+
+        TabulatedFunction multiplicationOfFunctions = service.multiplication(func1, func2);
+
+        assertEquals(3, multiplicationOfFunctions.getCount());
+        assertEquals(5.0, multiplicationOfFunctions.getY(0), 1e-10); // 5.0 * 1.0
+        assertEquals(20.0, multiplicationOfFunctions.getY(1), 1e-10); // 10.0 * 2.0
+        assertEquals(45.0, multiplicationOfFunctions.getY(2), 1e-10); // 15.0 * 3.0
+
+        TabulatedFunctionOperationService anotherService = new TabulatedFunctionOperationService(new LinkedListTabulatedFunctionFactory());
+
+        multiplicationOfFunctions = anotherService.multiplication(func1, func2);
+
+        assertEquals(3, multiplicationOfFunctions.getCount());
+        assertEquals(5.0, multiplicationOfFunctions.getY(0), 1e-10); // 5.0 * 1.0
+        assertEquals(20.0, multiplicationOfFunctions.getY(1), 1e-10); // 10.0 * 2.0
+        assertEquals(45.0, multiplicationOfFunctions.getY(2), 1e-10); // 15.0 * 3.0
+
+    }
+
+    @Test
+    void testMultiplicationWithLinkedListTabulatedFunctions() {
+        double[] xValues1 = {1.0, 2.0, 3.0};
+        double[] yValues1 = {5.0, 10.0, 15.0};
+        double[] xValues2 = {1.0, 2.0, 3.0};
+        double[] yValues2 = {1.0, 2.0, 3.0};
+
+        LinkedListTabulatedFunction func1 = new LinkedListTabulatedFunction(xValues1, yValues1);
+        LinkedListTabulatedFunction func2 = new LinkedListTabulatedFunction(xValues2, yValues2);
+
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService();
+
+        TabulatedFunction multiplicationOfFunctions = service.multiplication(func1, func2);
+
+        assertEquals(3, multiplicationOfFunctions.getCount());
+        assertEquals(5.0, multiplicationOfFunctions.getY(0), 1e-10); // 5.0 * 1.0
+        assertEquals(20.0, multiplicationOfFunctions.getY(1), 1e-10); // 10.0 * 2.0
+        assertEquals(45.0, multiplicationOfFunctions.getY(2), 1e-10); // 15.0 * 3.0
+
+        TabulatedFunctionOperationService anotherService = new TabulatedFunctionOperationService(new LinkedListTabulatedFunctionFactory());
+
+        multiplicationOfFunctions = anotherService.multiplication(func1, func2);
+
+        assertEquals(3, multiplicationOfFunctions.getCount());
+        assertEquals(5.0, multiplicationOfFunctions.getY(0), 1e-10); // 5.0 * 1.0
+        assertEquals(20.0, multiplicationOfFunctions.getY(1), 1e-10); // 10.0 * 2.0
+        assertEquals(45.0, multiplicationOfFunctions.getY(2), 1e-10); // 15.0 * 3.0
+
+    }
+
+    @Test
+    void testMultiplicationWithDifferentTabulatedFunctions1() {
+        double[] xValues1 = {1.0, 2.0, 3.0};
+        double[] yValues1 = {5.0, 10.0, 15.0};
+        double[] xValues2 = {1.0, 2.0, 3.0};
+        double[] yValues2 = {1.0, 2.0, 3.0};
+
+        ArrayTabulatedFunction func1 = new ArrayTabulatedFunction(xValues1, yValues1);
+        LinkedListTabulatedFunction func2 = new LinkedListTabulatedFunction(xValues2, yValues2);
+
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService();
+
+        TabulatedFunction multiplicationOfFunctions = service.multiplication(func1, func2);
+
+        assertEquals(3, multiplicationOfFunctions.getCount());
+        assertEquals(5.0, multiplicationOfFunctions.getY(0), 1e-10); // 5.0 * 1.0
+        assertEquals(20.0, multiplicationOfFunctions.getY(1), 1e-10); // 10.0 * 2.0
+        assertEquals(45.0, multiplicationOfFunctions.getY(2), 1e-10); // 15.0 * 3.0
+
+        TabulatedFunctionOperationService anotherService = new TabulatedFunctionOperationService(new LinkedListTabulatedFunctionFactory());
+
+        multiplicationOfFunctions = anotherService.multiplication(func1, func2);
+
+        assertEquals(3, multiplicationOfFunctions.getCount());
+        assertEquals(5.0, multiplicationOfFunctions.getY(0), 1e-10); // 5.0 * 1.0
+        assertEquals(20.0, multiplicationOfFunctions.getY(1), 1e-10); // 10.0 * 2.0
+        assertEquals(45.0, multiplicationOfFunctions.getY(2), 1e-10); // 15.0 * 3.0
+
+    }
+
+    @Test
+    void testMultiplicationWithDifferentTabulatedFunctions2() {
+        double[] xValues1 = {1.0, 2.0, 3.0};
+        double[] yValues1 = {5.0, 10.0, 15.0};
+        double[] xValues2 = {1.0, 2.0, 3.0};
+        double[] yValues2 = {1.0, 2.0, 3.0};
+
+        LinkedListTabulatedFunction func1 = new LinkedListTabulatedFunction(xValues1, yValues1);
+        ArrayTabulatedFunction func2 = new ArrayTabulatedFunction(xValues2, yValues2);
+
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService();
+
+        TabulatedFunction multiplicationOfFunctions = service.multiplication(func1, func2);
+
+        assertEquals(3, multiplicationOfFunctions.getCount());
+        assertEquals(5.0, multiplicationOfFunctions.getY(0), 1e-10); // 5.0 * 1.0
+        assertEquals(20.0, multiplicationOfFunctions.getY(1), 1e-10); // 10.0 * 2.0
+        assertEquals(45.0, multiplicationOfFunctions.getY(2), 1e-10); // 15.0 * 3.0
+
+        TabulatedFunctionOperationService anotherService = new TabulatedFunctionOperationService(new LinkedListTabulatedFunctionFactory());
+
+        multiplicationOfFunctions = anotherService.multiplication(func1, func2);
+
+        assertEquals(3, multiplicationOfFunctions.getCount());
+        assertEquals(5.0, multiplicationOfFunctions.getY(0), 1e-10); // 5.0 * 1.0
+        assertEquals(20.0, multiplicationOfFunctions.getY(1), 1e-10); // 10.0 * 2.0
+        assertEquals(45.0, multiplicationOfFunctions.getY(2), 1e-10); // 15.0 * 3.0
+
+    }
+
+    @Test
+    void testDevisionWithArrayTabulatedFunctions() {
+        double[] xValues1 = {1.0, 2.0, 3.0};
+        double[] yValues1 = {5.0, 10.0, 18.0};
+        double[] xValues2 = {1.0, 2.0, 3.0};
+        double[] yValues2 = {1.0, 2.0, 3.0};
+
+        ArrayTabulatedFunction func1 = new ArrayTabulatedFunction(xValues1, yValues1);
+        ArrayTabulatedFunction func2 = new ArrayTabulatedFunction(xValues2, yValues2);
+
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService();
+
+        TabulatedFunction devisionOfFunctions = service.devision(func1, func2);  // func1 / func2
+        assertEquals(3, devisionOfFunctions.getCount());
+        assertEquals(5.0, devisionOfFunctions.getY(0), 1e-10); // 5.0 / 1.0
+        assertEquals(5.0, devisionOfFunctions.getY(1), 1e-10); // 10.0 / 2.0
+        assertEquals(6.0, devisionOfFunctions.getY(2), 1e-10); // 18.0 / 3.0
+
+        devisionOfFunctions = service.devision(func2, func1);  // func2 / func1
+        assertEquals(3, devisionOfFunctions.getCount());
+        assertEquals(0.2, devisionOfFunctions.getY(0), 1e-4); // 1.0 / 5.0
+        assertEquals(0.2, devisionOfFunctions.getY(1), 1e-4); // 2.0 / 10.0
+        assertEquals(0.166666666, devisionOfFunctions.getY(2), 1e-4); // 3.0 / 18.0
+
+        TabulatedFunctionOperationService anotherService = new TabulatedFunctionOperationService(new LinkedListTabulatedFunctionFactory());
+
+        devisionOfFunctions = anotherService.devision(func1, func2);
+
+        assertEquals(3, devisionOfFunctions.getCount());
+        assertEquals(5.0, devisionOfFunctions.getY(0), 1e-10); // 5.0 / 1.0
+        assertEquals(5.0, devisionOfFunctions.getY(1), 1e-10); // 10.0 / 2.0
+        assertEquals(6.0, devisionOfFunctions.getY(2), 1e-10); // 18.0 / 3.0
+
+        devisionOfFunctions = service.devision(func2, func1);  // func2 / func1
+        assertEquals(3, devisionOfFunctions.getCount());
+        assertEquals(0.2, devisionOfFunctions.getY(0), 1e-4); // 1.0 / 5.0
+        assertEquals(0.2, devisionOfFunctions.getY(1), 1e-4); // 2.0 / 10.0
+        assertEquals(0.166666666, devisionOfFunctions.getY(2), 1e-4); // 3.0 / 18.0
+
+    }
+
+    @Test
+    void testDevisionWithLinkedListTabulatedFunctions() {
+        double[] xValues1 = {1.0, 2.0, 3.0};
+        double[] yValues1 = {5.0, 10.0, 18.0};
+        double[] xValues2 = {1.0, 2.0, 3.0};
+        double[] yValues2 = {1.0, 2.0, 3.0};
+
+        LinkedListTabulatedFunction func1 = new LinkedListTabulatedFunction(xValues1, yValues1);
+        LinkedListTabulatedFunction func2 = new LinkedListTabulatedFunction(xValues2, yValues2);
+
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService();
+
+        TabulatedFunction devisionOfFunctions = service.devision(func1, func2);
+        assertEquals(3, devisionOfFunctions.getCount());
+        assertEquals(5.0, devisionOfFunctions.getY(0), 1e-10); // 5.0 / 1.0
+        assertEquals(5.0, devisionOfFunctions.getY(1), 1e-10); // 10.0 / 2.0
+        assertEquals(6.0, devisionOfFunctions.getY(2), 1e-10); // 18.0 / 3.0
+
+        devisionOfFunctions = service.devision(func2, func1);  // func2 / func1
+        assertEquals(3, devisionOfFunctions.getCount());
+        assertEquals(0.2, devisionOfFunctions.getY(0), 1e-4); // 1.0 / 5.0
+        assertEquals(0.2, devisionOfFunctions.getY(1), 1e-4); // 2.0 / 10.0
+        assertEquals(0.166666666, devisionOfFunctions.getY(2), 1e-4); // 3.0 / 18.0
+
+        TabulatedFunctionOperationService anotherService = new TabulatedFunctionOperationService(new LinkedListTabulatedFunctionFactory());
+
+        devisionOfFunctions = anotherService.devision(func1, func2);
+        assertEquals(3, devisionOfFunctions.getCount());
+        assertEquals(5.0, devisionOfFunctions.getY(0), 1e-10); // 5.0 / 1.0
+        assertEquals(5.0, devisionOfFunctions.getY(1), 1e-10); // 10.0 / 2.0
+        assertEquals(6.0, devisionOfFunctions.getY(2), 1e-10); // 18.0 / 3.0
+
+        devisionOfFunctions = service.devision(func2, func1);  // func2 / func1
+        assertEquals(3, devisionOfFunctions.getCount());
+        assertEquals(0.2, devisionOfFunctions.getY(0), 1e-4); // 1.0 / 5.0
+        assertEquals(0.2, devisionOfFunctions.getY(1), 1e-4); // 2.0 / 10.0
+        assertEquals(0.166666666, devisionOfFunctions.getY(2), 1e-4); // 3.0 / 18.0
+
+    }
+
+    @Test
+    void testDivisionWithDifferentTabulatedFunctions1() {
+        double[] xValues1 = {1.0, 2.0, 3.0};
+        double[] yValues1 = {5.0, 10.0, 18.0};
+        double[] xValues2 = {1.0, 2.0, 3.0};
+        double[] yValues2 = {1.0, 2.0, 3.0};
+
+        LinkedListTabulatedFunction func1 = new LinkedListTabulatedFunction(xValues1, yValues1);
+        ArrayTabulatedFunction func2 = new ArrayTabulatedFunction(xValues2, yValues2);
+
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService();
+
+        TabulatedFunction devisionOfFunctions = service.devision(func1, func2);
+        assertEquals(3, devisionOfFunctions.getCount());
+        assertEquals(5.0, devisionOfFunctions.getY(0), 1e-10); // 5.0 / 1.0
+        assertEquals(5.0, devisionOfFunctions.getY(1), 1e-10); // 10.0 / 2.0
+        assertEquals(6.0, devisionOfFunctions.getY(2), 1e-10); // 18.0 / 3.0
+
+        devisionOfFunctions = service.devision(func2, func1);  // func2 / func1
+        assertEquals(3, devisionOfFunctions.getCount());
+        assertEquals(0.2, devisionOfFunctions.getY(0), 1e-4); // 1.0 / 5.0
+        assertEquals(0.2, devisionOfFunctions.getY(1), 1e-4); // 2.0 / 10.0
+        assertEquals(0.166666666, devisionOfFunctions.getY(2), 1e-4); // 3.0 / 18.0
+
+        TabulatedFunctionOperationService anotherService = new TabulatedFunctionOperationService(new LinkedListTabulatedFunctionFactory());
+
+        devisionOfFunctions = anotherService.devision(func1, func2);
+        assertEquals(3, devisionOfFunctions.getCount());
+        assertEquals(5.0, devisionOfFunctions.getY(0), 1e-10); // 5.0 / 1.0
+        assertEquals(5.0, devisionOfFunctions.getY(1), 1e-10); // 10.0 / 2.0
+        assertEquals(6.0, devisionOfFunctions.getY(2), 1e-10); // 18.0 / 3.0
+
+        devisionOfFunctions = service.devision(func2, func1);  // func2 / func1
+        assertEquals(3, devisionOfFunctions.getCount());
+        assertEquals(0.2, devisionOfFunctions.getY(0), 1e-4); // 1.0 / 5.0
+        assertEquals(0.2, devisionOfFunctions.getY(1), 1e-4); // 2.0 / 10.0
+        assertEquals(0.166666666, devisionOfFunctions.getY(2), 1e-4); // 3.0 / 18.0
+
+    }
+
+    @Test
+    void testDivisionWithDifferentTabulatedFunctions2() {
+        double[] xValues1 = {1.0, 2.0, 3.0};
+        double[] yValues1 = {5.0, 10.0, 18.0};
+        double[] xValues2 = {1.0, 2.0, 3.0};
+        double[] yValues2 = {1.0, 2.0, 3.0};
+
+        ArrayTabulatedFunction func1 = new ArrayTabulatedFunction(xValues1, yValues1);
+        LinkedListTabulatedFunction func2 = new LinkedListTabulatedFunction(xValues2, yValues2);
+
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService();
+
+        TabulatedFunction devisionOfFunctions = service.devision(func1, func2);
+        assertEquals(3, devisionOfFunctions.getCount());
+        assertEquals(5.0, devisionOfFunctions.getY(0), 1e-10); // 5.0 / 1.0
+        assertEquals(5.0, devisionOfFunctions.getY(1), 1e-10); // 10.0 / 2.0
+        assertEquals(6.0, devisionOfFunctions.getY(2), 1e-10); // 18.0 / 3.0
+
+        devisionOfFunctions = service.devision(func2, func1);  // func2 / func1
+        assertEquals(3, devisionOfFunctions.getCount());
+        assertEquals(0.2, devisionOfFunctions.getY(0), 1e-4); // 1.0 / 5.0
+        assertEquals(0.2, devisionOfFunctions.getY(1), 1e-4); // 2.0 / 10.0
+        assertEquals(0.166666666, devisionOfFunctions.getY(2), 1e-4); // 3.0 / 18.0
+
+        TabulatedFunctionOperationService anotherService = new TabulatedFunctionOperationService(new LinkedListTabulatedFunctionFactory());
+
+        devisionOfFunctions = anotherService.devision(func1, func2);
+        assertEquals(3, devisionOfFunctions.getCount());
+        assertEquals(5.0, devisionOfFunctions.getY(0), 1e-10); // 5.0 / 1.0
+        assertEquals(5.0, devisionOfFunctions.getY(1), 1e-10); // 10.0 / 2.0
+        assertEquals(6.0, devisionOfFunctions.getY(2), 1e-10); // 18.0 / 3.0
+
+        devisionOfFunctions = service.devision(func2, func1);  // func2 / func1
+        assertEquals(3, devisionOfFunctions.getCount());
+        assertEquals(0.2, devisionOfFunctions.getY(0), 1e-4); // 1.0 / 5.0
+        assertEquals(0.2, devisionOfFunctions.getY(1), 1e-4); // 2.0 / 10.0
+        assertEquals(0.166666666, devisionOfFunctions.getY(2), 1e-4); // 3.0 / 18.0
+
+    }
 }
