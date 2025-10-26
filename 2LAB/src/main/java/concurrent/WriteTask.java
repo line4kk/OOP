65ea050b516bc77c2c658a492e371bccacaa7d63
line4kk's book <silandr2016@gmail.com>
2025-10-26 14:48:34 +0400

    WriteTask class Поле TabulatedFunction у ReadTask теперь final

---
+++ 2LAB/src/main/java/concurrent/WriteTask.java
@@ -0,0 +1,21 @@
+package concurrent;
+
+import functions.TabulatedFunction;
+
+public class WriteTask implements Runnable {
+    private final TabulatedFunction tabulatedFunction;
+    private final double value;  // Значение, которое будет записываться в функцию для каждого x
+
+    public WriteTask(TabulatedFunction tabulatedFunction, double value) {
+        this.tabulatedFunction = tabulatedFunction;
+        this.value = value;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < tabulatedFunction.getCount() - 1; i++) {
+            tabulatedFunction.setY(i, value);
+            System.out.printf("Writing for index %d complete", i);
+        }
+    }
+}
